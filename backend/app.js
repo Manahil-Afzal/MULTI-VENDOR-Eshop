4971f993cc9246c04c2ba5ac401a770f9ef76dd5
Manahil Afzal <manahilafzal731@gmail.com>
2025-12-08 09:57:26 -0800

    Deployment

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://multi-vendor-eshop.vercel.app",
   credentials: true,
 }));
 
